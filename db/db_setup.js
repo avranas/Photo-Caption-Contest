1ec75947b277419a43edc8c89ab5c1ddbfb8d783
Alex Vranas <avranas42@gmail.com>
2022-08-02 08:13:00 -0700

    :(

--- db/db_setup.js
+++ db/db_setup.js
@@ -11,7 +11,7 @@ const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.D
       aquire: 30000,
       idle: 10000
    },
-   port: 5432,
+   port: process.env.DB_PORT,
    timestamps: false,
    freezeTableName: true
 });

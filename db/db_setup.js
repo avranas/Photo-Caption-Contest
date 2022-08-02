78a2abdc096ffb9a492e55d62b74b179e42770df
Alex Vranas <avranas42@gmail.com>
2022-08-02 08:06:08 -0700

    Changed host info in db_setup again

--- db/db_setup.js
+++ db/db_setup.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize');
 require("dotenv").config();
 
 const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-   host: process.env.DATABASE_URL,
+   host: process.env.DB_HOST,
    dialect: 'postgres',
    //I'm not sure if I need this, but it was in the tutorial
    pool: {
